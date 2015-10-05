0ba32b39a873e479335a0a92b8d2b89a4b36c7f7
kevin.forshey@gmail.com <kevin.forshey@gmail.com>
2015-10-05 09:12:26 -0400

    Adding script to add non-free source to sources.list

---
+++ addtesting.py
@@ -0,0 +1,3 @@
+import os
+os.system("echo '### non-free added by DebAwesome'>> /etc/apt/sources.list")
+os.system("echo 'deb http://ftp.au.debian.org/debian testing contrib non-free' >> /etc/apt/sources.list")
